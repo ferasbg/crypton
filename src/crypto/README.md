87598b086bcb025898c9a87abf68b611e67c07c4
Feras Baig <ferasbaig@gmail.com>
2021-01-10 18:33:46 -0600

    update segnet

---
+++ src/crypto/README.md
@@ -0,0 +1,6 @@
+# crypton.src.crypto
+ 
+
+## Class Components
+- `crypton.src.crypto.mpc`
+- `crypton.src.crypto.ot`
--- src/prediction/model.py
+++ src/prediction/model.py
@@ -7,9 +7,25 @@ import time
 from tqdm import tqdm
 from collections import deque
 
+
 from tensorflow.keras import Model, Sequential
 from tensorflow.keras.layers import Dense, Embedding, Reshape
 from tensorflow.keras.optimizers import Adam
 
+import torch
+from torch import nn
+import torchvision.models as models
+
+class Model(nn.Module):
+    def __init__(self, classes=19):
+        # recursive inheritance
+        super(Model, self).__init__()
+
+    def build_model(self):
+        # call torch network constructor
+        network = self.models.vgg16(pretrained=True)
 
 
+if __name__ == '__main__':
+    model = Model()
+    model.summary()
