8d702a3740499cf6cff66a1a1f99d084203bcdba
Feras Baig <ferasbaig@gmail.com>
2021-01-10 20:53:45 -0600

    update

--- src/prediction/nn.py
+++ src/prediction/nn.py
@@ -16,11 +16,21 @@ import torchvision.models as models
 from PIL import Image
 from torchvision import transforms
 
+import logging
+
+# configure to DEBUG state for logging level, format log records (data, time, and message)
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
+
+# options: FileHandler, StreamHandler, HTTPHandler, NullHandler, SMTPHandler, SocketHandler, MemoryHandler
+
+
+
+
 class Prediction(models.VGG):
     def __init__(self, model='vgg16', requires_grad=True, remove_fc=True, show_params=False):
         # recursive inheritance
         super(Prediction, self).__init__()
-        
+
 
     def build_model(self):
         # call torch network constructor
