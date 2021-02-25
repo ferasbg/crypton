37c4f25b58b86259c1d3e221fb13f3479c4f861e
Feras Baig <ferasbaig@gmail.com>
2021-02-24 19:06:39 -0600

    update

---
+++ src/adversarial/adversarial.py
@@ -0,0 +1,9 @@
+
+
+class Adversarial():
+    @staticmethod
+    def create_adversarial_example():
+        # create adversarial example with perturbations to input_image iterating over entire train set to then pass into perturbed_train_generator
+        raise NotImplementedError
+
+
