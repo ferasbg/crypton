47d87f959d41dae1b7acd2333f6a17f51da71cec
Feras Baig <ferasbaig@gmail.com>
2021-02-26 17:32:54 -0600

    update

--- src/verification/specification.py
+++ src/verification/specification.py
@@ -77,9 +77,33 @@ class RobustnessTrace():
         References:
         Examples:
     '''
-    raise NotImplementedError
+    affine_output_vector = False
+    adversarial_sample_created = False
+
+    @staticmethod
+    def affine_abstract_output_vector_state():
+        # return : bool
+        # if vector is inside robustness region, then specification is met
+        raise NotImplementedError
+
+    @staticmethod
+    def adversarial_sample_not_created():
+        # return : bool
+        # if the perturbations don't change the output label for pixelwise_perturbation_ɛ = 0.03, then there is no adversarial_sample (evaluate given dict_string of image_label for cifar-10)
+        raise NotImplementedError
+
+
 
 
 if __name__ == '__main__':
     Specification()
+    t_f = RobustnessTrace.affine_abstract_output_vector_state()
+    adv = RobustnessTrace.adversarial_example_not_created()
+    if (t_f == False and adv == False):
+        return True
+    else if (t_f == False):
+        affine_output_vector = True
+    else if (adv == False):
+        adversarial_sample_created = True
+
 
