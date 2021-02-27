a37c4491949c160de2b7b03c85ea258d57eaa3f3
Feras Baig <ferasbaig@gmail.com>
2021-02-27 12:32:32 -0600

    update

--- src/prediction/metrics.py
+++ src/prediction/metrics.py
@@ -7,11 +7,15 @@ import argparse
 import h5py
 import json
 
+"""
+
+Define conventional/nominal, certification/verification (robustness, safety), mpc, adversarial evaluation metrics for network.
+
+
+"""
 
 class Metrics():
     def __init__(self):
-        # iou
-        self.mean_iou = 0
         self.true_positive_pixels = 0
         self.false_positive_pixels = 0
         self.false_negative_pixels = 0
@@ -19,42 +23,18 @@ class Metrics():
 
         # acc
         self.mean_acc = 0
-        self.pixelwise_acc = 0
 
         # perturbation
         self.gaussian_epsilon = 0.10
         self.perturbation_epsilon = 0.05
 
     @staticmethod
-    def compute_mi_fgsm(self, adversarial_sample_size, correct_adversarial_labels):
-        raise NotImplementedError
-
-    @staticmethod
     def top_1(self):
         raise NotImplementedError
 
     @staticmethod
-    def top_5(self):
-        raise NotImplementedError
-
-    @staticmethod
-    def cross_entropy_loss(self):
+    def nominal_cross_entropy_loss(self):
         """ Compute Cross-Entropy Loss Given compute_softmax(Model model) """
-        raise NotImplementedError
-
-    @staticmethod
-    def adjusted_rand_index(self, true_mask, pred_mask, name='ari_score'):
-        """Compute Adjusted Rand Score (ARI) which is a clustering similarity score.
-        """
-        raise NotImplementedError
-
-
-    @staticmethod
-    def mean_intersection_over_union(self):
-        raise NotImplementedError
-
-    @staticmethod
-    def frequency_weighted_iou():
         raise NotImplementedError
 
     @staticmethod
@@ -85,3 +65,19 @@ class Metrics():
     def per_class(self):
         """Get Per-Class (PC) Accuracy for Correctly Labeled Pixels for Each Class, then Average Over Total Classes"""
         raise NotImplementedError
+
+
+    @staticmethod
+    def certified_robustness_accuracy(prediction_robustness_threshold, certified_accuracy):
+        """robustSize(scores, , δ, L) returns the certified robustness size, which is then compared to the prediction robustness threshold T."""
+        certified_robustness_size = {}
+        if (certified_robustness_size > prediction_robustness_threshold):
+            return certified_robustness_size
+
+    @staticmethod
+    def nominal_kl_divergence():
+        raise NotImplementedError
+
+
+
+
