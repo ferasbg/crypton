f6b2702210ea8ec5ad8416688935f499d47cc965
Feras Baig <ferasbaig@gmail.com>
2021-06-15 21:48:14 -0500

    update

--- src/federated/flower/run.sh
+++ src/federated/flower/run.sh
@@ -1,17 +1,14 @@
+NUM_CLIENTS=100
+
 python3 server.py & 
 sleep 4 
 
-python3 client.py --partition=0 &
-python3 client.py --partition=1 &
-python3 client.py --partition=2 &
-python3 client.py --partition=3 &
-python3 client.py --partition=4 &
-python3 client.py --partition=5 &
-python3 client.py --partition=6 &
-python3 client.py --partition=7 &
-python3 client.py --partition=8 &
-python3 client.py --partition=9 &
-python3 client.py --partition=10 &
+for ((i = 0; i < $NUM_CLIENTS; i++))
+do
+    echo "Starting client(cid=$i) with partition $i out of $NUM_CLIENTS clients."
+    python3 client.py & 
+done
+echo "Started $NUM_CLIENTS clients."
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 sleep 86400 # this is 24 hours
